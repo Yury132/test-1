8b2458defca4ad8388d8e4e023e2e0d41aa3a6f0
Yury132 <usynin.yury@yandex.ru>
2023-10-24 21:20:03 +0300

    new goose
    new goose

--- cmd/migrations/0001_users.sql
+++ cmd/migrations/0001_users.sql
@@ -1,10 +1,10 @@
 -- +goose Up
 create table if not exists public.service_user
 (
-    id         bigint       not null primary key,
+    id         serial not null primary key,
     name       varchar(100) not null,
     email        varchar(100) not null
 );
 
 -- +goose Down
-DROP TABLE public.service_user;
+drop table public.service_user;
