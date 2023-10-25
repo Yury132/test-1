10b486fbf8a36306c127cd311e7dcdb3b4234fd5
Yury132 <usynin.yury@yandex.ru>
2023-10-25 09:29:44 +0300

    go run main.go
    go run main.go

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -24,6 +23,7 @@ import (
 const (
 	dialect  = "pgx"
 	dbString = "host=localhost user=root password=mydbpass dbname=mydb port=5432 sslmode=disable"
+	command  = "up"
 )
 
 var (
@@ -36,31 +36,31 @@ var (
 func main() {
 
 	//---------------------------новое --------------------------------
-	flags.Usage = usage
-	flags.Parse(os.Args[1:])
+	// flags.Usage = usage
+	// flags.Parse(os.Args[1:])
 
-	args := flags.Args()
-	if len(args) == 0 || args[0] == "-h" || args[0] == "--help" {
-		flags.Usage()
-		return
-	}
-	command := args[0]
+	// args := flags.Args()
+
+	// if len(args) == 0 || args[0] == "-h" || args[0] == "--help" {
+	// 	flags.Usage()
+	// 	return
+	// }
+
+	//command := args[0]
 
 	db, err := goose.OpenDBWithDriver(dialect, dbString)
 	if err != nil {
-		fmt.Println("123")
 		log.Fatalf(err.Error())
 	}
 
 	defer func() {
 		if err := db.Close(); err != nil {
-			fmt.Println("321")
 			log.Fatalf(err.Error())
 		}
 	}()
 
-	if err := goose.Run(command, db, *dir, args[1:]...); err != nil {
-		fmt.Println("5555")
+	//if err := goose.Run(command, db, *dir, args[1:]...); err != nil {
+	if err := goose.Run(command, db, *dir); err != nil {
 		log.Fatalf("migrate %v: %v", command, err)
 	}
 
@@ -105,31 +105,31 @@ func main() {
 }
 
 // ---------------------------новое -------------------
-func usage() {
-	fmt.Println(usagePrefix)
-	flags.PrintDefaults()
-	fmt.Println(usageCommands)
-}
+// func usage() {
+// 	fmt.Println(usagePrefix)
+// 	flags.PrintDefaults()
+// 	fmt.Println(usageCommands)
+// }
 
-var (
-	usagePrefix = `Usage: migrate COMMAND
-Examples:
-    migrate status
-`
+// var (
+// 	usagePrefix = `Usage: migrate COMMAND
+// Examples:
+//     migrate status
+// `
 
-	usageCommands = `
-Commands:
-    up                   Migrate the DB to the most recent version available
-    up-by-one            Migrate the DB up by 1
-    up-to VERSION        Migrate the DB to a specific VERSION
-    down                 Roll back the version by 1
-    down-to VERSION      Roll back to a specific VERSION
-    redo                 Re-run the latest migration
-    reset                Roll back all migrations
-    status               Dump the migration status for the current DB
-    version              Print the current version of the database
-    create NAME [sql|go] Creates new migration file with the current timestamp
-    fix                  Apply sequential ordering to migrations`
-)
+// 	usageCommands = `
+// Commands:
+//     up                   Migrate the DB to the most recent version available
+//     up-by-one            Migrate the DB up by 1
+//     up-to VERSION        Migrate the DB to a specific VERSION
+//     down                 Roll back the version by 1
+//     down-to VERSION      Roll back to a specific VERSION
+//     redo                 Re-run the latest migration
+//     reset                Roll back all migrations
+//     status               Dump the migration status for the current DB
+//     version              Print the current version of the database
+//     create NAME [sql|go] Creates new migration file with the current timestamp
+//     fix                  Apply sequential ordering to migrations`
+// )
 
 //---------------------------новое --------------------------------
