4a434d677a0dc1fdeaabd441be2c245394e59cb1
Yury132 <usynin.yury@yandex.ru>
2023-10-25 11:40:49 +0300

    dir
    dir

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"flag"
 	"log"
 	"os"
 	"os/signal"
@@ -19,35 +18,16 @@ import (
 	"github.com/pressly/goose/v3"
 )
 
-// ---------------------------новое --------------------------------
 const (
 	dialect  = "pgx"
 	dbString = "host=localhost user=root password=mydbpass dbname=mydb port=5432 sslmode=disable"
 	command  = "up"
+	newDir   = "migrations"
 )
-
-var (
-	flags = flag.NewFlagSet("migrate", flag.ExitOnError)
-	dir   = flags.String("dir", "migrations", "directory with migration files")
-)
-
-//---------------------------новое --------------------------------
 
 func main() {
 
-	//---------------------------новое --------------------------------
-	// flags.Usage = usage
-	// flags.Parse(os.Args[1:])
-
-	// args := flags.Args()
-
-	// if len(args) == 0 || args[0] == "-h" || args[0] == "--help" {
-	// 	flags.Usage()
-	// 	return
-	// }
-
-	//command := args[0]
-
+	// Миграции
 	db, err := goose.OpenDBWithDriver(dialect, dbString)
 	if err != nil {
 		log.Fatalf(err.Error())
@@ -59,12 +39,12 @@ func main() {
 		}
 	}()
 
-	//if err := goose.Run(command, db, *dir, args[1:]...); err != nil {
-	if err := goose.Run(command, db, *dir); err != nil {
+	// newDir Как правильно указать путь? в том числе к шаблонам html
+	if err := goose.Run(command, db, newDir); err != nil {
 		log.Fatalf("migrate %v: %v", command, err)
 	}
 
-	//---------------------------новое -------------------
+	// Конфигурации
 	cfg, err := config.Parse()
 	if err != nil {
 		panic(err)
@@ -103,33 +83,3 @@ func main() {
 
 	<-shutdown
 }
-
-// ---------------------------новое -------------------
-// func usage() {
-// 	fmt.Println(usagePrefix)
-// 	flags.PrintDefaults()
-// 	fmt.Println(usageCommands)
-// }
-
-// var (
-// 	usagePrefix = `Usage: migrate COMMAND
-// Examples:
-//     migrate status
-// `
-
-// 	usageCommands = `
-// Commands:
-//     up                   Migrate the DB to the most recent version available
-//     up-by-one            Migrate the DB up by 1
-//     up-to VERSION        Migrate the DB to a specific VERSION
-//     down                 Roll back the version by 1
-//     down-to VERSION      Roll back to a specific VERSION
-//     redo                 Re-run the latest migration
-//     reset                Roll back all migrations
-//     status               Dump the migration status for the current DB
-//     version              Print the current version of the database
-//     create NAME [sql|go] Creates new migration file with the current timestamp
-//     fix                  Apply sequential ordering to migrations`
-// )
-
-//---------------------------новое --------------------------------
